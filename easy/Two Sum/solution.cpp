4e0e8e9a24bc9770068def630e0bb6c8e918a72e
Ishraaq Parvez <i4ishraaq@gmail.com>
2022-07-28 12:13:55 +0530

    Update solution.cpp

--- easy/Two Sum/solution.cpp
+++ easy/Two Sum/solution.cpp
@@ -1,1 +1,14 @@
-
+vector<int> twoSum(vector<int>& nums, int target) {
+  // we will use the two pointers method after sorting the array
+  
+  // first we will sort the array to ensure that 
+  // the array is increasing
+  sort(nums.begin(), nums.end()); 
+  int l=0, r=nums.size()-1; 
+  while(l<=r){
+    if(arr[l]+arr[r]==target){
+      
+    }
+    
+  }
+}
