abe8a5771f7d04a276487e44ac802ee94916e6c4
Ishraaq Parvez <i4ishraaq@gmail.com>
2023-08-08 23:19:26 +0530

    Time: 3 ms (76.59%) | Memory: 11.1 MB (13.70%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -3,16 +3,14 @@ public:
     int search(vector<int>& nums, int target) {
         int l=0, r=size(nums)-1;
         while(l<=r){
-            int mid = l+(r-l)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
+            int mid = (l+r)/2;
+        
             if(nums[mid]>nums.back()){
                 l=mid+1; 
             }
             else r=mid-1; 
         }
-        printf("%d", l);
+        // printf("%d", l);
         if(binary_search(begin(nums), begin(nums)+l, target)){
             return distance(begin(nums), lower_bound(begin(nums), begin(nums)+l, target)); 
         }
