752240a835a8f152c8058d9bbd426683570b79c5
Ishraaq Parvez <i4ishraaq@gmail.com>
2023-08-10 09:46:23 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (88.75%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        for(int& I:nums){
-            if(I==target){
-                return true; 
-            }
-        }
-        return 0;
+        return find(begin(nums), end(nums), target)!=end(nums);
     }
 };
