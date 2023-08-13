dd60c44a09c8449c67080a67b344d2dda512e895
Ishraaq Parvez <i4ishraaq@gmail.com>
2023-08-13 09:25:44 +0530

    Time: 136 ms (79.69%) | Memory: 84.1 MB (85.85%) - LeetSync

--- 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -4,14 +4,8 @@ public:
         vector<bool> dp(size(nums)+1, false); dp[0]=true; 
         nums.insert(begin(nums), 0); 
         for(int i=2;i<size(dp);i++){
-            if(nums[i]==nums[i-1]){
-                dp[i]=dp[i]||dp[i-2]; 
-            }
-            if(i>=3){
-                if((nums[i]==nums[i-1] && nums[i-1]==nums[i-2]) || (nums[i]==nums[i-1]+1 && nums[i-1]==nums[i-2]+1)){
-                    dp[i]= dp[i-3]||dp[i];
-                }
-            }
+            dp[i]=dp[i] || (nums[i]==nums[i-1] && dp[i-2]) || (i>=3 && (((nums[i]==nums[i-1] && nums[i-1]==nums[i-2]) || (nums[i]==nums[i-1]+1 && nums[i-1]==nums[i-2]+1)) && dp[i-3]));
+            
 
         }
         return dp.back();
